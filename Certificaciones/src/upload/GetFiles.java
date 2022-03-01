f7fb9c1df14bfc364091fa8c254bf56c80d78cd7
danielcont <contrerasgz.daniel@gmail.com>
2022-03-01 16:30:03 -0600

    Structure Modified to Support List Accessory

--- Certificaciones/src/upload/GetFiles.java
+++ Certificaciones/src/upload/GetFiles.java
@@ -1,5 +1,5 @@
-/*
- * This Class is made to import files from the computer and get the data from it
+ /*
+  *  This Class is made to import files from the computer and get the data from it
  */
 
 package upload;
@@ -13,6 +13,7 @@ import java.io.File;
 
 // GUI Components Libraries are imported
 import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -20,7 +21,7 @@ import javax.swing.JLabel;
 import javax.swing.JList;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.ListSelectionModel;
+import javax.swing.SwingUtilities;
 
 public class GetFiles extends JFrame implements ActionListener {
 	
@@ -30,12 +31,9 @@ public class GetFiles extends JFrame implements ActionListener {
 	private JButton navigate_button, process_button; // Button To Navigate Folder and Process Files
 	// JPanel
 	private JPanel load_panel, list_panel;
-	// JScrollPane
-	private JScrollPane scroll_pane;
-	// File Object
-	private File[] files;
-	// JList of Files
-	private JList listFiles;
+	// JList
+	private JList<File> fileList;
+	private DefaultListModel<File> fileListModel;
 	
 	// Main
 	public static void main(String[] args) {
@@ -47,6 +45,13 @@ public class GetFiles extends JFrame implements ActionListener {
 	// GUI
 	public void UploadFiles() {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JFileChooser fc = new JFileChooser();
+        FileListAccessory accessory = new FileListAccessory(fc);
+        DefaultListModel model = accessory.getModel();
+
+        fileListModel = new DefaultListModel<>();
+		fileList = new JList(fileListModel);
 		
 		// Create 'Choose File' Panel
 		load_panel = new JPanel() {
@@ -65,7 +70,31 @@ public class GetFiles extends JFrame implements ActionListener {
 		// SEARCH FILES Button
 		navigate_button = new JButton("Buscar...");
 		load_panel.add(navigate_button);
-		navigate_button.addActionListener(this); // File Navigator Functionality Added To This Button
+		navigate_button.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+
+		        SwingUtilities.invokeLater(new Runnable() {
+		            @Override
+		            public void run() {
+		                fc.setAccessory(accessory);
+		                fc.setMultiSelectionEnabled(true);
+
+		                int open = fc.showOpenDialog(fc);
+		                if (open == JFileChooser.APPROVE_OPTION) {
+		                	fileListModel.removeAllElements();
+		                	
+		                	if (model.getSize() == 0) process_button.setEnabled(false);
+		                	else process_button.setEnabled(true);
+		                	
+		                    for (int i = 0; i < model.getSize(); i++) {
+		                        fileListModel.addElement(((File)model.getElementAt(i)));
+		                    }
+		                }
+		            }
+		        });
+			}
+		}); // File Navigator Functionality Added To This Button
 		
 
 		// Create 'List Files' Panel
@@ -75,22 +104,20 @@ public class GetFiles extends JFrame implements ActionListener {
 				return new Dimension (450, 600);
 			};
 		};
-		this.getContentPane().add(list_panel);
 		list_panel.setBorder(BorderFactory.createTitledBorder("Lista de Archivos Seleccionados"));
 		
-		
+		list_panel.add(new JScrollPane(fileList), BorderLayout.NORTH);
 		
 		// Process Button
 		process_button = new JButton("Procesar");
-		list_panel.add(process_button, BorderLayout.SOUTH);
+		list_panel.add(process_button);
 		process_button.addActionListener(this);
-		if(files == null) process_button.setEnabled(false);
-		
+		if(fileListModel.isEmpty()) process_button.setEnabled(false);
 		
 		// Add Panels to Frame and Assign Them a Position
 		frame.setVisible(true);
-		frame.setSize(450, 680);
-		frame.setMinimumSize(new Dimension(450, 480));
+		frame.setSize(350, 350);
+		frame.setMinimumSize(new Dimension(350, 350));
 		frame.setLocationRelativeTo(null);
 		
 		frame.add(load_panel, BorderLayout.NORTH);
@@ -99,33 +126,6 @@ public class GetFiles extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == navigate_button) {
-			JFileChooser file_chooser = new JFileChooser();
-			
-			file_chooser.setMultiSelectionEnabled(true); // Multiple Files Selection is Enabled
-			int result = file_chooser.showSaveDialog(this);
-			files = file_chooser.getSelectedFiles();
-			
-			
-			// File List Shows Files
-			if (result == JFileChooser.APPROVE_OPTION) {
-				
-				process_button.setEnabled(true);
-				
-				listFiles = new JList<>(files);
-				
-				listFiles.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
-				scroll_pane = new JScrollPane(listFiles);
-				list_panel.add(scroll_pane, BorderLayout.NORTH);
-				
-				System.out.println(files.length);
-				
-				frame.invalidate();
-				frame.validate();
-				frame.repaint();
-				
-			}
-		}
 			
 	}
 	
