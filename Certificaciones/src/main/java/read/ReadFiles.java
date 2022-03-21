ed5078b8f82f0eaea9c9bbe9cad5c9e3d5892363
danielcont <contrerasgz.daniel@gmail.com>
2022-03-21 16:39:58 -0600

    'Processing' Dialog is added

--- Certificaciones/src/main/java/read/ReadFiles.java
+++ Certificaciones/src/main/java/read/ReadFiles.java
@@ -4,11 +4,15 @@
 
 package read;
 
+import java.awt.Dimension;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import javax.swing.DefaultListModel;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
 
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -16,21 +20,42 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ReadFiles {
 
-	public void readFiles(DefaultListModel<File> model) throws IOException {
+	private void readFiles(DefaultListModel<File> model) throws IOException {
 		
 		// Start Reading the File
-		
+
 		// 1. Files
 		for(int i = 0; i < model.getSize(); i++) {
 			// Is Important to Know the # of Workbooks
 			XSSFWorkbook workbook = new XSSFWorkbook(new FileInputStream(model.getElementAt(i).toString()));
 			// 2. Sheets Within Workbooks are Read
-			for(int j = 0; j < workbook.getNumberOfSheets(); i++) {
+			for(int j = 0; j < workbook.getNumberOfSheets(); j++) {
 				XSSFSheet sheet = workbook.getSheetAt(j);
 				System.out.println("hi");
 			}
 		}
 		
+	}
+	
+	public void WaitingUI(DefaultListModel<File> model) {
+		JDialog dialog = new JDialog();
+		JLabel label = new JLabel("Por Favor Espere");
+		dialog.setSize(250, 150);
+		dialog.setTitle("Procesando...");
+		dialog.add(label);
+		dialog.setMinimumSize(new Dimension(250, 150));
+		dialog.setLocationRelativeTo(null);
+		
+		dialog.setVisible(true);
+
+        try {
+			readFiles(model);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		dialog.setVisible(false);
 	}
 
 }
